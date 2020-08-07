13bafb1efe9e85f4b1b934752a4b4834b7c43de6
Grant Montgomery <grantm@rusondia.net>
2020-08-06 23:32:23 -0500

    moved global variables to a new class.

---
+++ lib/globals.dart
@@ -0,0 +1,8 @@
+bool doLegality = false;
+bool doPeerCells = true;
+bool doPeerDigits = true;
+int maxHints = 5;
+int selectedRow = -1;
+int selectedCol = -1;
+List hintsGiven = [];
+
--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/services.dart';
 import 'CustomStyles.dart';
 import 'domains/sudoku/SudokuProblem.dart';
 import 'domains/sudoku/SudokuState.dart';
+import 'globals.dart' as globals;
 import 'dart:ui';
 import 'dart:math';
 
@@ -41,22 +43,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  SudokuProblem problem = SudokuProblem();
-  GridView board;
-  bool cellSelected = false;
+  SudokuProblem _problem = SudokuProblem();
+//  GridView board;
   var menuHeight = 70;
-  int selectedRow = 0;
-  int selectedCol = 0;
-  SolvingAssistant assistant;
-  int hintsLeft = 5;
-  List hintsGiven = [];
+//  int _selectedRow = -1;
+//  int _selectedCol = -1;
+  SolvingAssistant _assistant;
+//  int globals.maxHints;
+//  List globals.hintsGiven = [];
+//  bool globals.doLegality = false;
+//  bool globals.doPeerCells = true;
+//  bool globals.doPeerDigits = false;
 
   void _resetBoard() {
-    problem = SudokuProblem();
-    cellSelected = false;
+    _problem = SudokuProblem();
+    globals.selectedRow = -1;
+    globals.selectedCol = -1;
     setState(() {});
-    hintsLeft = 5;
-    hintsGiven.clear();
+    globals.hintsGiven.clear();
   }
 
   double getConstraint() {
@@ -70,9 +74,9 @@ class _MyHomePageState extends State<MyHomePage> {
     return constraint;
   }
 
-  EdgeInsets getBoardPadding(int index) {
-    int row = index ~/ problem.board_size;
-    int col = index % problem.board_size;
+  EdgeInsets _getBoardPadding(int index) {
+    int row = index ~/ _problem.board_size;
+    int col = index % _problem.board_size;
 
     double thickness = 1.5;
     double defaultThickness = 0.5;
@@ -87,10 +91,10 @@ class _MyHomePageState extends State<MyHomePage> {
     if (col == 0) {
       left = thickness;
     }
-    if (row % problem.cell_size == problem.cell_size - 1) {
+    if (row % _problem.cell_size == _problem.cell_size - 1) {
       bottom = thickness;
     }
-    if (col % problem.cell_size == problem.cell_size - 1) {
+    if (col % _problem.cell_size == _problem.cell_size - 1) {
       right = thickness;
     }
 
@@ -105,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   bool _givenAsHint(int row, int col) {
     bool hint = false;
-    for(List pair in hintsGiven) {
+    for(List pair in globals.hintsGiven) {
       if(pair[0] == row && pair[1] == col) {
         hint = true;
       }
@@ -114,46 +118,45 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _giveHint() {
-    if (!problem.success() && hintsLeft > 0) {
-      SudokuState currentState = problem.getCurrentState();
+    if (!_problem.success() && _getHintsLeft() > 0) {
+      SudokuState currentState = _problem.getCurrentState();
       List currentBoard = currentState.getTiles();
-      SudokuState finalState = problem.getFinalState();
+      SudokuState finalState = _problem.getFinalState();
       List finalBoard = finalState.getTiles();
       var pos1;
       var pos2;
       do {
-        pos1 = _getRandom(problem.board_size);
-        pos2 = _getRandom(problem.board_size);
+        pos1 = _getRandom(_problem.board_size);
+        pos2 = _getRandom(_problem.board_size);
       } while(currentBoard[pos1][pos2] != 0);
       var num = finalBoard[pos1][pos2];
       _doMove(num, pos1, pos2);
       setState(() {
-        selectedRow = pos1;
-        selectedCol = pos2;
-        hintsGiven.add([pos1, pos2]);
-        hintsLeft--;
+        globals.selectedRow = pos1;
+        globals.selectedCol = pos2;
+        globals.hintsGiven.add([pos1, pos2]);
       });
     }
   }
 
   void _doMove(int num, int row, int col) {
-    assistant = SolvingAssistant(problem);
-    SudokuState initialState = problem.getInitialState();
+    _assistant = SolvingAssistant(_problem);
+    SudokuState initialState = _problem.getInitialState();
     var initialBoard = initialState.getTiles();
     var notInitialHint = initialBoard[row][col] == 0;
-    if (!problem.success() && notInitialHint) {
+    if (!_problem.success() && notInitialHint) {
       var move = 'Place ' +
           num.toString() +
           ' at ' +
           row.toString() +
           ' ' +
           col.toString();
-      assistant.tryMove(move);
+      _assistant.tryMove(move);
     }
   }
 
   void _solveGame(SudokuProblem problem) {
-    int cell_size = problem.cell_size;
+//    int cell_size = problem.cell_size;
     int board_size = problem.board_size;
     for (var i = 0; i < board_size; i++) {
       for (var j = 0; j < board_size; j++) {
@@ -169,29 +172,60 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  bool _cellSelected() {
+    return globals.selectedRow != -1 && globals.selectedCol != -1;
+  }
+
+  int _getHintsLeft() {
+    print('maxHints: ' + globals.maxHints.toString());
+    print('globals.hintsGiven: ' + globals.hintsGiven.toString());
+    print('globals.hintsGiven.length: ' + globals.hintsGiven.length.toString());
+    var hintsLeft = globals.maxHints - globals.hintsGiven.length;
+    return hintsLeft;
+  }
+
   Color _getCellColor(int row, int col) {
     Color peerCell = CustomStyles.frost[1];
-    Color backgroud = CustomStyles.snowStorm[2];
+    Color background = CustomStyles.snowStorm[2];
     Color peerDigit = CustomStyles.frost[2];
-    Color color;
-    if (cellSelected == null) {
-      cellSelected = false;
+    Color color = background;
+
+    if(row == globals.selectedRow && col == globals.selectedCol) {
+      color = peerDigit;
     }
-    if (cellSelected) {
-      if (row == selectedRow || col == selectedCol) {
-        color = peerCell;
+
+    if(globals.doPeerCells) {
+      if (_cellSelected()) {
+        if (row == globals.selectedRow || col == globals.selectedCol) {
+          color = peerCell;
+        }
+        if (row ~/ _problem.cell_size == globals.selectedRow ~/ _problem.cell_size &&
+            col ~/ _problem.cell_size == globals.selectedCol ~/ _problem.cell_size) {
+          color = peerCell;
+        }
+        if (row == globals.selectedRow && col == globals.selectedCol) {
+          color = background;
+        }
+      } else {
+        color = background;
       }
-      if (row ~/ problem.cell_size == selectedRow ~/ problem.cell_size &&
-          col ~/ problem.cell_size == selectedCol ~/ problem.cell_size) {
-        color = peerCell;
-      }
-      if (row == selectedRow && col == selectedCol) {
-        color = backgroud;
-      }
-    } else {
-      color = backgroud;
     }
-    if (problem.success()) {
+
+    if(globals.doPeerDigits) {
+      SudokuState currentState = _problem.getCurrentState();
+      List currentBoard = currentState.getTiles();
+      if(_cellSelected()) {
+        if(currentBoard[row][col] != 0 && currentBoard[row][col] == currentBoard[globals.selectedRow][globals.selectedCol]) {
+          color = peerDigit;
+        }
+      }
+    }
+
+    if(globals.doPeerCells && globals.doPeerDigits && row == globals.selectedRow && col == globals.selectedCol) {
+      color = background;
+    }
+
+    if (_problem.success()) {
       color = peerCell;
     }
     return color;
@@ -206,16 +240,15 @@ class _MyHomePageState extends State<MyHomePage> {
     String toPlace = cellNum == 0 ? '' : cellNum.toString();
 
     Ink button = Ink(
-      padding: getBoardPadding(index),
+      padding: _getBoardPadding(index),
       child: Material(
         color: _getCellColor(row, col),
         child: InkWell(
           splashColor: CustomStyles.frost[2],
           hoverColor: CustomStyles.frost[3],
           onTap: () {
-            selectedRow = row;
-            selectedCol = col;
-            cellSelected = true;
+            globals.selectedRow = row;
+            globals.selectedCol = col;
             setState(() {});
           },
           child: Center(
@@ -235,33 +268,58 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    if (problem == null) {
-      problem = SudokuProblem();
+    globals.maxHints = 5;
+//    globals.doPeerDigits = true;
+    if (_problem == null) {
+      _problem = SudokuProblem();
     }
 
     return Scaffold(
       backgroundColor: CustomStyles.snowStorm[2],
       appBar: AppBar(
         leading: Container(
-//          child: Material(
           child: InkWell(
-//              splashColor: Colors.deepOrange,
             child: Container(
               height: 30,
               width: 30,
               child: Icon(Icons.menu, color: CustomStyles.snowStorm[2], size: 30),
             ),
             onTap: () {
-              _solveGame(problem);
+              _solveGame(_problem);
               setState(() {});
             },
           ),
-//          ),
         ),
         title: Text(
           'LettuceSudoku',
           textAlign: TextAlign.center,
           style: CustomStyles.titleText,
+        ),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Center(
+                child: Text('Settings',
+                  style: CustomStyles.titleText,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: CustomStyles.polarNight[3],
+              ),
+            ),
+            Flex(
+              direction: Axis.horizontal,
+              children: [
+                Row(
+                children: [
+                  Text('Highlight Peer Cells'),
+                ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
       body: _getBody(),
@@ -275,11 +333,11 @@ class _MyHomePageState extends State<MyHomePage> {
         color: CustomStyles.polarNight[3],
         child: GridView.count(
             padding: EdgeInsets.all(1),
-            crossAxisCount: problem.board_size,
+            crossAxisCount: _problem.board_size,
             childAspectRatio: 1,
             children:
-                List.generate(problem.board_size * problem.board_size, (index) {
-              Ink button = _makeBoardButton(index, problem);
+                List.generate(_problem.board_size * _problem.board_size, (index) {
+              Ink button = _makeBoardButton(index, _problem);
               return button;
             })),
       ),
@@ -295,7 +353,7 @@ class _MyHomePageState extends State<MyHomePage> {
       mainAxisSpacing: 2,
       crossAxisSpacing: 2,
       children: List.generate(10, (index) {
-        int num = (index + 1) % (problem.board_size + 1);
+        int num = (index + 1) % (_problem.board_size + 1);
         String toPlace = num == 0 ? 'X' : (index + 1).toString();
         Container button = Container(
           child: Material(
@@ -304,8 +362,8 @@ class _MyHomePageState extends State<MyHomePage> {
               hoverColor: Colors.grey,
               splashColor: Colors.grey,
               onTap: () {
-                if(cellSelected) {
-                  _doMove(num, selectedRow, selectedCol);
+                if(_cellSelected()) {
+                  _doMove(num, globals.selectedRow, globals.selectedCol);
                   setState(() {});
                 }
               },
@@ -327,7 +385,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Color _getTextColor(int row, int col) {
-    SudokuState initialState = problem.getInitialState();
+    SudokuState initialState = _problem.getInitialState();
     var initialBoard = initialState.getTiles();
     var initialHint = initialBoard[row][col] != 0;
     Color color = CustomStyles.frost[3];
@@ -356,6 +414,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _makeBoardCol() {
+    var hintsLeft = _getHintsLeft();
     Column col = Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
@@ -366,7 +425,6 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Container(
               padding: EdgeInsets.all(4),
               child: Container(
-//                color: Colors.black,
                 child: _getBoard(),
               ),
             ),
@@ -375,29 +433,21 @@ class _MyHomePageState extends State<MyHomePage> {
         Flexible(
           flex: 2,
           fit: FlexFit.tight,
-//          color: Colors.red,
           child: Container(
-//            color: Colors.black,
             padding: EdgeInsets.all(4),
             child: Center(
               child: Container(
-//                color: Colors.blue,
                 child: _getMoveButtons(),
               ),
             ),
           ),
         ),
         Container(
-//          height: 120,
           child: Flex(
             mainAxisAlignment: MainAxisAlignment.center,
             direction: Axis.horizontal,
             children: <Widget>[
               Flexible(
-//                flex: 3,
-//                padding: EdgeInsets.only(right: 16),
-//                child: Material(
-//                  color: CustomStyles.snowStorm[2],
                 fit: FlexFit.tight,
                   child: FlatButton(
                     hoverColor: CustomStyles.snowStorm[0],
@@ -412,7 +462,6 @@ class _MyHomePageState extends State<MyHomePage> {
                       style: CustomStyles.getFiraCode(CustomStyles.polarNight[3], 26),
                     ),
                   ),
-//                ),
               ),
               Flexible(
 //                flex: 2,
@@ -424,7 +473,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       _giveHint();
                     },
                     child: Text(
-                      'hint ($hintsLeft)',
+                      'hint(' + hintsLeft.toString() + ')',
                       textAlign: TextAlign.center,
                       maxLines: 1,
                       style: CustomStyles.getFiraCode(CustomStyles.polarNight[3], 26),
