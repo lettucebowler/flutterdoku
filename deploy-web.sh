8c6b03603cf3bab228da6ad6dd209b63e404ed23
Grant Montgomery <grantm@rusondia.net>
2020-08-11 23:38:47 -0500

    added icons for app on android, ios, and web. modified deploy-web.sh to add the icon to the website

--- deploy-web.sh
+++ deploy-web.sh
@@ -1,3 +1,7 @@
 flutter build web
+cp icon/icon.png icon/favicon.png
+mv icon/favicon.png build/web
+cp icon/Icon-192.png build/web/icons
+cp icon/Icon-512.png build/web/icons
 cd build/web
 scp -r * rusondia.net:/home/lettucebowler/sites/sudoku
