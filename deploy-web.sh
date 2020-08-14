c75bbae63976a65bc55b855df19b0afe3a6f0471
Grant Montgomery <grantm@rusondia.net>
2020-08-14 18:11:03 -0500

    fixed deploy-web.sh

--- deploy-web.sh
+++ deploy-web.sh
@@ -1,7 +1,6 @@
 flutter build web --dart-define=FLUTTER_WEB_USE_SKIA=true
-cp icon/icon.png icon/favicon.png
-mv icon/favicon.png build/web
-cp icon/Icon-192.png build/web/icons
-cp icon/Icon-512.png build/web/icons
+cp assets/icon/icon.png build/web/favicon.png
+cp assets/icon/Icon-192.png build/web/icons
+cp assets/icon/Icon-512.png build/web/icons
 cd build/web
 scp -r * rusondia.net:/home/lettucebowler/sites/sudoku
