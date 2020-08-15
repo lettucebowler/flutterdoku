9cebf31411485226d6b1ba8730c0a9f944545f27
Grant Montgomery <grantm@rusondia.net>
2020-08-15 17:30:12 -0500

    got rid of maps because I couldn't figure out how to make it work for now

--- lib/util/globals.dart
+++ lib/util/globals.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:lettuce_sudoku/domains/sudoku/SudokuProblem.dart';
 
 // BoolWrapper doLegality = BoolWrapper(false);
@@ -14,33 +14,9 @@ double bodySpacing = 4;
 List hintsGiven = [];
 SudokuProblem problem;
 
-var settingsMap = {
-  'doPeerCells': doPeerCells,
-  'doPeerDigits': doPeerDigits,
-  'doMistakes': doMistakes,
-  'initialHints': initialHints,
-  'legalityRadio': legalityRadio
-};
-
-// class BoolWrapper {
-//   bool value;
-//   BoolWrapper(bool value) {
-//     this.value = value;
-//   }
-// }
-
-// class IntWrapper extends VariableWrapper{
-//   // int value;
-//   IntWrapper(int value) {
-//     this.value = value;
-//   }
-// }
-
 class VariableWrapper {
   var value;
   VariableWrapper(var value) {
     this.value = value;
   }
 }
-
-Widget mistakesWidget;
