37fdf4e2d790ac24d208fd4e871f89df4e99c156
Grant Montgomery <grantm@rusondia.net>
2020-08-19 00:34:46 -0500

    fixed deploy-web.sh

--- deploy-web.sh
+++ deploy-web.sh
@@ -1,4 +1,6 @@
+
 flutter build web --dart-define=FLUTTER_WEB_USE_SKIA=true
+mkdir build/web/icons
 cp assets/icon/icon.png build/web/favicon.png
 cp assets/icon/Icon-192.png build/web/icons
 cp assets/icon/Icon-512.png build/web/icons
