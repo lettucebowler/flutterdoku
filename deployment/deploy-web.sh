c2cdb2d962ac18f64d25a9cc05d0c96e4ee9bd0d
Grant Montgomery <grantm@rusondia.net>
2021-09-17 23:41:52 -0500

    update deployment script

--- deployment/deploy-web.sh
+++ deployment/deploy-web.sh
@@ -4,4 +4,4 @@ cp assets/icon/icon.png build/web/favicon.png
 cp assets/icon/Icon-192.png build/web/icons
 cp assets/icon/Icon-512.png build/web/icons
 cd build/web
-scp -r * rusondia.net:/opt/docker/volumes/sudoku/www
+scp -r * rusondia.net:/opt/docker/volumes/sudoku/config/www
